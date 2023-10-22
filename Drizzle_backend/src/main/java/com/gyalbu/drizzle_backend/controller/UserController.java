7b15a03119569c27456297783baa95e06ec5490d
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-10-22 16:11:19 +0545

    user controller

--- Drizzle_backend/src/main/java/com/gyalbu/drizzle_backend/controller/UserController.java
+++ Drizzle_backend/src/main/java/com/gyalbu/drizzle_backend/controller/UserController.java
@@ -1,15 +1,27 @@
 package com.gyalbu.drizzle_backend.controller;
 
+import com.gyalbu.drizzle_backend.entity.User;
+import com.gyalbu.drizzle_backend.exception.UserException;
 import com.gyalbu.drizzle_backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfileHandler(@RequestHeader("Authorization") String jwt) throws UserException {
+
+        User user = userService.findUserProfileByJwt(jwt);
+        return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
+    }
 }
