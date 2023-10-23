115283a64fff51df44fd42f23b91e9555e9b8648
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-10-23 12:00:34 +0545

    slider setup

--- Drizzle_frontend/src/app/home/home.component.ts
+++ Drizzle_frontend/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {menJeans} from "../../Data/Men/men_jeans";
+import {gounsPage1} from "../../Data/Gouns/gouns";
+import {lehngacholiPage2} from "../../Data/Saree/lenghaCholiPage2";
+import {mensShoesPage1} from "../../Data/shoes";
 
 @Component({
   selector: 'app-home',
@@ -7,8 +11,14 @@ import {Component} from '@angular/core';
 })
 export class HomeComponent {
   Iphone: any;
+  Laptop: any;
+  Camera: any;
+  Printer: any;
 
-  ngOnInit(){
-
+  ngOnInit() {
+    this.Iphone = menJeans.splice(0, 5);
+    this.Laptop = gounsPage1.splice(0, 5);
+    this.Camera = lehngacholiPage2.splice(0, 5);
+    this.Printer = mensShoesPage1.splice(0, 5);
   }
 }
