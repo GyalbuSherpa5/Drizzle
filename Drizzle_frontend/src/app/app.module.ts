2b64946e5983a0d1435cc6c17fbe9673af605ee9
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-10-23 11:27:17 +0545

    home setup

--- Drizzle_frontend/src/app/app.module.ts
+++ Drizzle_frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { MainCarouselComponent } from './home/main-carousel/main-carousel.component';
+import { ProductSliderComponent } from './home/product-slider/product-slider.component';
+import { HomeProductCardComponent } from './home/home-product-card/home-product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainCarouselComponent,
+    ProductSliderComponent,
+    HomeProductCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
