b5de785d9874ef2021b16cd8ca1a34de5b6d76b8
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-11-12 13:33:40 +0545

    frontend

---
+++ Frontend_Drizzle/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainCarouselComponent } from './home/main-carousel/main-carousel.component';
+import { HomeProductCardComponent } from './home/home-product-card/home-product-card.component';
+import { ProductSliderComponent } from './home/product-slider/product-slider.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavContentComponent } from './navbar/nav-content/nav-content.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MainCarouselComponent,
+    HomeProductCardComponent,
+    ProductSliderComponent,
+    NavbarComponent,
+    NavContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Frontend_Drizzle/src/app/navbar/nav-content/nav-content.ts
@@ -0,0 +1,100 @@
+export const navigation = {
+  smart_phone: {
+    id: 'women',
+    name: 'Women',
+    featured: [
+      {
+        name : 'New Arrivals',
+        href: '/',
+        imageSrc: "",
+        imageAlt: "put image ok"
+      },
+      {
+        name : 'Basic Tees',
+        href: '/',
+        imageSrc: "",
+        imageAlt: "put image ok"
+      },
+    ],
+    sections:[
+      {
+        id: 'clothing',
+        name: 'Clothing',
+        items:[
+          {name:'Tops',id:"top",href: "#"},
+          {name:'Dresses',id:"top",href: "#"},
+          {name:'Pants',id:"top",href: "#"},
+        ]
+      },
+      {
+        id: 'accessories',
+        name: 'Accessories',
+        items:[
+          {name:'Watches',id:"top",href: "#"},
+          {name:'Wallets',id:"top",href: "#"},
+          {name:'Bags',id:"top",href: "#"},
+          {name:'Sun glasses',id:"top",href: "#"},
+        ]
+      },
+      {
+        id: 'brands',
+        name: 'Brands',
+        items:[
+          {name:'Re-Arranged',id:"top",href: "#"},
+          {name:'Counter felt',id:"top",href: "#"},
+          {name:'Full Nelson',id:"top",href: "#"},
+          {name:'My was',id:"top",href: "#"},
+        ]
+      }
+    ]
+  },
+  laptop: {
+    id: 'men',
+    name: 'Men',
+    featured: [
+      {
+        name : 'New Arrivals',
+        href: '/',
+        imageSrc: "",
+        imageAlt: "put image ok"
+      },
+      {
+        name : 'Basic Tees',
+        href: '/',
+        imageSrc: "",
+        imageAlt: "put image ok"
+      },
+    ],
+    sections:[
+      {
+        id: 'clothing man',
+        name: 'Clothing',
+        items:[
+          {name:'Tops',id:"top",href: "#"},
+          {name:'Dresses',id:"top",href: "#"},
+          {name:'Pants',id:"top",href: "#"},
+        ]
+      },
+      {
+        id: 'accessories',
+        name: 'Accessories',
+        items:[
+          {name:'Watches',id:"top",href: "#"},
+          {name:'Wallets',id:"top",href: "#"},
+          {name:'Bags',id:"top",href: "#"},
+          {name:'Sun glasses',id:"top",href: "#"},
+        ]
+      },
+      {
+        id: 'brands',
+        name: 'Brands',
+        items:[
+          {name:'Re-Arranged',id:"top",href: "#"},
+          {name:'Counter felt',id:"top",href: "#"},
+          {name:'Full Nelson',id:"top",href: "#"},
+          {name:'My was',id:"top",href: "#"},
+        ]
+      }
+    ]
+  }
+}
