51870453b8244057c433624e454298785a528b9c
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2024-01-13 16:54:52 +0545

    admin routing

--- Frontend_Drizzle/src/app/Module/admin/admin.module.ts
+++ Frontend_Drizzle/src/app/Module/admin/admin.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './components/admin.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { OrderTableComponent } from './components/order-table/order-table.component';
-import { CustomersComponent } from './components/customers/customers.component';
-import { AdminProductsComponent } from './components/admin-products/admin-products.component';
-import { CreateProductComponent } from './components/create-product/create-product.component';
+import {AdminRoutingModule} from './admin-routing.module';
+import {AdminComponent} from './components/admin.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {OrderTableComponent} from './components/order-table/order-table.component';
+import {CustomersComponent} from './components/customers/customers.component';
+import {AdminProductsComponent} from './components/admin-products/admin-products.component';
+import {CreateProductComponent} from './components/create-product/create-product.component';
 
 
 @NgModule({
--- Frontend_Drizzle/src/app/Module/admin/components/admin.component.ts
+++ Frontend_Drizzle/src/app/Module/admin/components/admin.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AdminComponent {
 
+  model: any[] = [];
+
+  ngOnInit() {
+    this.updateMenu();
+  }
+
+  updateMenu() {
+    this.model = [
+      {
+        label: '',
+        items: [
+          {
+            label: 'Home',
+            icon: 'pi pi-home',
+            // command: () => this.home()
+          },
+          {
+            label: 'Orders',
+            icon: 'pi pi-arrow-circle-up',
+            routerLink: ['orders'],
+          },
+          {
+            label: 'Products',
+            icon: 'pi pi-arrow-circle-up',
+            routerLink: ['products'],
+          },
+          {
+            label: 'Customers',
+            icon: 'pi pi-spin pi-cog',
+            routerLink: ['customers'],
+          }
+          ,
+          {
+            label: 'Create Product',
+            icon: 'pi pi-database',
+            routerLink: ['create-product'],
+          }
+        ]
+      }
+    ];
+  }
+
 }
