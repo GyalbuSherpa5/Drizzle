d7f772a61b9d81a89a218becb6b33b16c7da4085
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2024-03-02 18:46:08 +0545

    kyc verification form

--- Frontend_Drizzle/src/app/Module/auth/user-kyc/user-kyc.component.ts
+++ Frontend_Drizzle/src/app/Module/auth/user-kyc/user-kyc.component.ts
@@ -12,6 +12,11 @@ export class UserKycComponent {
 
   date: Date | undefined;
   occupations: any;
+  documentType: any;
+  issuedAddress: any;
+  zone: any;
+  district: any;
+  municipality: any;
 
   genderOptions: any[] = [
     { label: 'Male', value: 'male' },
@@ -44,6 +49,20 @@ export class UserKycComponent {
     citizenBack: ['', Validators.required],
   });
 
+  fileToUpload!: File;
+  file_name: string = '';
+
+  fileToUploadB!: File;
+  file_nameB: string = '';
+
+  onFileChange(event: Event) {
+    this.prepareUpload(event);
+  }
+
+  onFileChangeB(event: Event) {
+    this.prepareUploadB(event);
+  }
+
   constructor(
     private fb: FormBuilder,
     private messageService: MessageService,
@@ -58,9 +77,153 @@ export class UserKycComponent {
       { title: 'Accountant', code: 'ACC' },
       { title: 'Chef', code: 'CHF' }
     ];
+
+    this.documentType = [
+      { title: 'Citizenship', code: 'C' },
+    ];
+
+    this.issuedAddress = [
+      { title: 'Dolakha', code: 'D' },
+      { title: 'Kathmandu', code: 'K' },
+      { title: 'Lalitpur', code: 'L' },
+      { title: 'Bhaktapur', code: 'B' },
+      { title: 'Pokhara', code: 'P' }
+    ];
+
+    this.zone = [
+      { title: 'Bagmati', code: 'B' },
+      { title: 'Gandaki', code: 'G' },
+      { title: 'Lumbini', code: 'L' },
+      { title: 'Karnali', code: 'K' },
+      { title: 'Sudurpashchim', code: 'S' }
+    ];
+
+    this.district = [
+      { title: 'Dolakha', code: 'D' },
+      { title: 'Kathmandu', code: 'K' },
+      { title: 'Lalitpur', code: 'L' },
+      { title: 'Bhaktapur', code: 'B' },
+      { title: 'Pokhara', code: 'P' }
+    ];
+
+    this.municipality = [
+      { title: 'Damak Municipality', code: 'D' },
+      { title: 'Biratnagar Metropolitan City', code: 'B' },
+      { title: 'Dharan Sub-Metropolitan City', code: 'D' },
+      { title: 'Itahari Sub-Metropolitan City', code: 'I' },
+      { title: 'Biratchowk Rural Municipality', code: 'B' }
+    ];
   }
 
   handleSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    } else {
+    }
+  }
 
+  prepareUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.fileToUpload = input.files[0];
+      this.file_name = this.fileToUpload.name;
+      const originalContentType = this.fileToUpload.type;
+      const contentType = originalContentType.split('/')[0];
+
+      // Check if the file is an image or video
+      if (contentType === 'image' || contentType === 'video') {
+        console.log(contentType);
+
+        if (contentType === 'image') {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            // @ts-ignore
+            this.imagePreview = e.target?.result;
+          };
+          reader.readAsDataURL(this.fileToUpload);
+        }
+      } else {
+        console.log('Invalid file type. Only image and video files are allowed.');
+        this.resetFileInput(); // Reset the input field or handle the error as appropriate.
+      }
+    }
+  }
+
+  resetFileInput() {
+    this.file_name = '';
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      this.fileToUpload = files[0];
+      this.file_name = this.fileToUpload.name;
+    }
+  }
+
+
+  prepareUploadB(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.fileToUploadB = input.files[0];
+      this.file_nameB = this.fileToUploadB.name;
+      const originalContentType = this.fileToUploadB.type;
+      const contentType = originalContentType.split('/')[0];
+
+      // Check if the file is an image or video
+      if (contentType === 'image' || contentType === 'video') {
+        console.log(contentType);
+
+        if (contentType === 'image') {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            // @ts-ignore
+            this.imagePreview = e.target?.result;
+          };
+          reader.readAsDataURL(this.fileToUploadB);
+        }
+      } else {
+        console.log('Invalid file type. Only image and video files are allowed.');
+        this.resetFileInputB(); // Reset the input field or handle the error as appropriate.
+      }
+    }
+  }
+
+  resetFileInputB() {
+    this.file_nameB = '';
+  }
+
+  onDragOverB(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDragLeaveB(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  onDropB(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      this.fileToUploadB = files[0];
+      this.file_nameB = this.fileToUploadB.name;
+    }
   }
 }
